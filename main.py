8d236c549dd127a009b0031193065cab66c44581
Rohit Natesh <rohit.natesh95@gmail.com>
2023-12-22 01:33:04 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# TODO: 1. Get token.
+# TODO: 2. Get people list.
+# TODO: 3. Read grants excel.
+# TODO: 4. Keep rows which we are interested in.
+# TODO: 5. Make entries for grant_award table. (If get all is done, mark the unpaired awards as inactive)
+# TODO: 6. Make entries for the grant_award table.
+# TODO: 7. Write the data.
